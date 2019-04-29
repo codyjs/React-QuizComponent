d69d139d3bfa1cf83316a11b9ab8c3871c647f77
Cody Simms <csimms@netspend.com>
2019-04-29 15:20:00 -0500

    added incorrect warning

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false
+    }
+  }
+
   handleClick(buttonText) {
-      if (buttonText === this.props.quiz_question.answer) {
-          this.props.showNextQuestionHandler();
-      }
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+      this.setState({
+        incorrectAnswer: false
+      });
+    } else {
+      this.setState({
+        incorrectAnswer: true
+      });
+    }
   }
 
   render() {
@@ -17,10 +31,15 @@ class QuizQuestion extends Component {
         <section className="buttons">
           <ul>
             {this.props.quiz_question.answer_options.map((ans, index) => (
-              <QuizQuestionButton key={index} button_text={ans} clickHandler={this.handleClick.bind(this)}/>
+              <QuizQuestionButton
+                key={index}
+                button_text={ans}
+                clickHandler={this.handleClick.bind(this)}
+              />
             ))}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     );
   }
